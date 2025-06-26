7c5f1fff9ea83f15185a58f0192d9483d640cae7
kxnnxthmxlgxs <73911764+kxnnxthmxlgxs@users.noreply.github.com>
2025-06-26 23:21:50 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import sweetviz
+import os
+
+st.set_page_config(page_title="Auto EDA Tool", layout="wide")
+st.title("Auto EDA Tool")
+st.write("Upload your CSV file and get a full EDA report instanatly.")
+
+Uploaded_file = st.file_uploader("Upload your CSV", type="csv")
+
+if Uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.write("### Data Preview", df.head())
+    
+    if st.button("Generate EDA Report"):
+        report = sweetviz.analyze(df)
+        report_path = "EDA_Report.html"
+        report.show_html(report_path, open_browser=False)
+        
+        with open(report_path, "r", encoding="utf-8") as f:
+            html_content = f.read()
+            st.components.v1.html(html_content, height=1000, scrolling=True)
